d516dc2cdd0525b6d1a3e82b1a65660c7eb05cd5
zlei <674846286@qq.com>
2026-03-26 22:02:42 +0800

    no

---
+++ tDemo/tDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tDemo
+//
+//  Created by lei on 2026/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tDemo/tDemo/tDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tDemoApp.swift
+//  tDemo
+//
+//  Created by lei on 2026/3/26.
+//
+
+import SwiftUI
+
+@main
+struct tDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
